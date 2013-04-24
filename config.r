e5e347ba2a03e86421a8672578fc2a18a5dfc065
Basil Veerman <bveerman@uvic.ca>
2013-04-23 17:12:44 -0700

    updates for new grass7 syntax changes

--- config.r
+++ config.r
@@ -1,7 +1,11 @@
 # This is the config file to set parameters before running the associated parts.
 # Any neccessary options must be here
 
-outdir <- '.'
+
+# You should not need to change the following lines.  They are for data setup only.
+
+
+outdir <- '/home/data/projects/hydrology/vic/data/routomator/output'
 verbose <- TRUE
 method = 2 #choose method 1 or 2 for creating flow accumulation raster for flowgen.c
 # What watershed are we calculating for?
@@ -12,7 +16,8 @@ watershed.isnew <- FALSE
 #~ watershed <- 'peace'
 #~ subbasins <- c('AKIEC','BCPCN','GRAHM','HALGR','KWADA','MOBER','MURWV','NATMO','OSILI','PACKM','PEACT','PINPN','BCGMS','FINAK','HALFA','INGEN','MESIL','MURMO','NATFT','OMNOS','OSPIK','PARMS','PEAPN','SUKMO')
 
-# GRASS7 settings
-gisBase <- "/home/bveerman/download/grass_trunk/dist.x86_64-unknown-linux-gnu"
-gisDbase <- '/home/bveerman/grassdata'
-location <- 'vic_routing'
+# GRASS7 settings - You should not need to change these
+
+gisBase <- "/home/bveerman/downloads/grass7_trunk/dist.x86_64-unknown-linux-gnu"
+gisDbase <- '/home/data/projects/hydrology/vic/data/routomator/grassdata'
+location <- 'test7'
--- grass_prep.r
+++ grass_prep.r
@@ -1,12 +1,10 @@
 library(spgrass6)
 
-# Load in existing location
-# grass64
-#loc <- initGRASS("/usr/lib/grass64", override=TRUE, home='/home/bveerman/', gisDbase='/home/bveerman/grassdata', location='vic_routing', mapset='PERMANENT')
-execGRASS("g.mapset", parameters=list(mapset="PERMANENT"))
-# grass7
-loc <- initGRASS(gisBase="/home/bveerman/download/grass_trunk/dist.x86_64-unknown-linux-gnu", home='/home/bveerman/', gisDbase='/home/bveerman/grassdata', location='vic_routing', mapset='PERMANENT', override=TRUE)
+source('config.r')
 
+
+# Load in existing location
+loc <- initGRASS(gisBase=gisBase, home='/home/bveerman/', gisDbase=gisDbase, location=location, mapset='PERMANENT', override=TRUE)
 
 # load DEM
 dem <- "/home/data/gis/basedata/DEMS/SRTM_DEM/SRTM_CSI_CGIAR_v4_1/srtm_bc_buffered.tif"
@@ -15,17 +13,12 @@ execGRASS("g.region", parameters=list(rast="dem"))
 execGRASS("r.info", parameters=list(map="dem"))
 
 # aggregate DEM to 15as
-execGRASS("g.region", parameters=list(res='00:00:15'))
+execGRASS("g.region", parameters=list(w='-139', s='47', n='61', e='-114', nsres='0.004166667', ewres='0.004166667'))
 execGRASS("r.resamp.stats", parameters=list(input='dem', output= 'dem-15'))
 
 # pull in region information from raster
-execGRASS("g.region", flags='l')
-execGRASS("g.region", parameters=list(rast="dem"))
-
-# set up region to aggregate raster into
-execGRASS("g.region", parameters=list(w='139:00:00W', s='47:00:00N', n='61:00:00N', e='114:00:00W', nsres='00:03:45', ewres='00:03:45'))
-execGRASS("g.region", parameters=list(w='139:00:00W', s='47:00:00N', n='61:00:00N', e='114:00:00W', nsres='00:00:15', ewres='00:00:15'))
-
+#execGRASS("g.region", flags='l')
+#execGRASS("g.region", parameters=list(rast="dem"))
 
 # make sure hydrodem extension installed
 execGRASS('g.extension', flags=c('a', 's'), parameters=list(extension='r.hydrodem'))
