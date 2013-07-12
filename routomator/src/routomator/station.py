9537650aa55982d3eafa83580e600fbdf6f274e1
Basil Veerman <bveerman@uvic.ca>
2013-07-11 17:41:25 -0700

    Refactor to use (xi, yi) and (lat,lon) stanards, coord functions to source from raster object

--- routomator/src/routomator/station.py
+++ routomator/src/routomator/station.py
@@ -7,25 +7,17 @@ class Station(object):
         self.lat = lat
         self.lon = lon
 
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__)
+                and self.__dict__ == other.__dict__)
+
     def raster_coords(self, r):
         '''
         Converts station lat/lon to raster xi/yi (anchor upper left) based on the
         metadata of an input raster
         '''
 
-        max_lon = r.xll + (r.ncols * r.cellsize)
-        max_lat = r.yll + (r.nrows * r.cellsize)
-        # print "{} < {} < {}: {}".format(self.xll, lon, max_lon, self.xll < lon < max_lon)
-        # print "{} < {} < {}: {}".format(self.yll, lat, max_lat, self.yll < lat < max_lat)
-
-        if not r.xll < lon < max_lon:
-            raise ValueError('Given lon not contained in raster, lon must be between {} and {}, given {}'.format(r.xll, max_lon, lon))
-        if not r.yll < lat < max_lat:
-            raise ValueError('Given lat not contained in raster, lat must be between {} and {}, given {}'.format(r.yll, max_lat, lat))
-
-        xi = next(i for i,v in enumerate(r.x_bnds) if v + r.cellsize > lon)
-        yi = next(i for i,v in enumerate(r.y_bnds) if v + r.cellsize > lat)
-        return (xi, yi)
+        return r.raster_coords((self.lat, self.lon))
         
     def vic_coords(self, r):
         '''
@@ -33,22 +25,25 @@ class Station(object):
         metadata of an input raster
         '''
 
-        (xi, yi) = raster_coords(r)
-        return (xi, r.nrows - (yi+1))
+        return r.vic_coords((self.lat, self.lon))
 
-def load_stations(station_list):
+def load_stations(station_csv):
     '''
     Loads the station list from an open file like object
     '''
     import csv
-    s = csv.DictReader(station_list)
+    with open(station_csv, 'r') as f:
+        s = csv.DictReader(f)
     
-    stations = []
-    for row in s:
-        stations.append(Station(row['STATION'], float(row['LAT']), float(row['LONG'])))
+        stations = []
+        for row in s:
+            stations.append(Station(row['STATION'], float(row['LAT']), float(row['LONG'])))
     return stations
 
-def validate_station_names(station_list):
+def generate_shortnames(station_list):
+    '''
+    Documentations...
+    '''
     raise NotImplemented
 
 if __name__ == '__main__':
