8e86e0612d5f02a9c78e2636f8250663e0d24cd3
Basil Veerman <bveerman@uvic.ca>
2013-07-30 08:48:50 -0700

    Refactor script/src of flow_dir_correct

---
+++ routomator/src/routomator/direction.py
@@ -0,0 +1,55 @@
+from raster import AsciiRaster
+
+class BreakIt(Exception):
+    # Custom exception class to break out of nested loop
+    pass
+
+def find_direction((x,y), (i,j)):
+    '''
+    GRASS directions
+    8   1   2
+    7   x   3
+    6   5   4
+    '''
+
+    if i < x:
+        if j < y: return '6'
+        if j == y: return '7'
+        if j > y: return '8'
+    if i == x:
+        if j < y: return '5'
+        if j > y: return '1'
+    if i > x:
+        if j < y: return '4'
+        if j == y: return '3'
+        if j > y: return '2'
+
+def find_neighbors(dirs, x, y):
+    yr = range(y-1, y+2)
+    xr = range(x-1, x+2)
+    neighbors = [(i, j) for j in yr for i in xr if (i != x or j != y)]
+    return neighbors
+
+def correct_edge_flows(r):
+
+    # first find all flows with -9 direction
+    invalids = []
+    for i, row in enumerate(r.raster):
+        for j, value in enumerate(row):
+            if value == '-9':
+                invalids.append((i, j))
+
+    # fix all invalid values
+    for x, y in invalids:
+        try:
+            neighbors = find_neighbors(r.raster, x, y)
+
+            for i, j in neighbors:
+                try:
+                    if r.raster[i][j] != '0':
+                        r.raster[x][y] = find_direction((x,y),(i,j))
+                        raise BreakIt # Use custom excpetion to jump out of nested loop
+                except IndexError:
+                    continue
+        except BreakIt:
+            pass
--- routomator/src/routomator/flow_dir_correct.py
+++ routomator/src/routomator/flow_dir_correct.py
@@ -2,82 +2,23 @@ import os
 import argparse
 
 from raster import AsciiRaster
+from routomator.direction import correct_edge_flows
 
-class BreakIt(Exception):
-    # Custom exception class to break out of nested loop
-    pass
 
-def find_invalids():
-    pass
 
-def find_direction((x,y), (i,j)):
-    '''
-    GRASS directions
-    8   1   2
-    7   x   3
-    6   5   4
-    '''
-
-    if i < x:
-        if j < y: return '6'
-        if j == y: return '7'
-        if j > y: return '8'
-    if i == x:
-        if j < y: return '5'
-        if j > y: return '1'
-    if i > x:
-        if j < y: return '4'
-        if j == y: return '3'
-        if j > y: return '2'
-
-def find_neighbors(dirs, x, y, verbose=False):
-    yr = range(y-1, y+2)
-    xr = range(x-1, x+2)
-    neighbors = [(i, j) for j in yr for i in xr if (i != x or j != y)]
-    
-    if verbose:
-        for j in yr:
-            print [dirs[i][j] for i in xr]
-
-    return neighbors
 
 def main(args):
     if args.watershed is None: assert 'Watershed argument missing'
 
-    r = Raster()
-    r.load_ascii(args.input)
+    r = AsciiRaster(args.input)
     
     # find all invalid values
-    invalids = []
-    for i, row in enumerate(r.raster):
-        for j, value in enumerate(row):
-            if value == '-9':
-                invalids.append((i, j))
-
-    if args.verbose:
-        print invalids
-
-    # fix all invalid values
-    for x, y in invalids:
-        try:
-            if args.verbose:
-                print 'Fixing direction {} found at {}, {}'.format(r.raster[x][y],x, y)
-            neighbors = find_neighbors(r.raster, x, y, args.verbose)
-
-            for i, j in neighbors:
-                try:
-                    if r.raster[i][j] != '0':
-                        r.raster[x][y] = find_direction((x,y),(i,j))
-                        raise BreakIt # Use custom excpetion to jump out of nested loop
-                except IndexError:
-                    continue
-        except BreakIt:
-            pass
-
+    correct_edge_flows(r)
+ 
     print 'Invalid Directions Fixed'
 
     outfile = os.path.join(args.outdir, args.watershed, 'flow-dir-16th-corrected.asc')
-    r.save_ascii(outfile)
+    r.save(outfile)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Flow direction corrector')
